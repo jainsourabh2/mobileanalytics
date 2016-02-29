caa4f8e5bbf9890d4868068eb35329d2dfa06d73
Sourabh Jain <jainsourabh2@gmail.com>
2016-02-29 14:53:21 +0530

    Merged Crash Changes

--- config/config.js
+++ config/config.js
@@ -8,3 +8,4 @@ module.exports.tbl_usereventinfo = 'user_event_info';
 module.exports.tbl_userhourlysessioninfo = 'user_hourly_session_info';
 module.exports.tbl_userhourlyeventinfo = 'user_hourly_event_info';
 module.exports.tbl_realtime_data = 'real_time_data';
+module.exports.tbl_crash_data = 'crash_data';
