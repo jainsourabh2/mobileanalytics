5df8de9b7cd60b061417532a14ba12138f68fc3c
Sourabh Jain <jainsourabh2@gmail.com>
2016-03-04 18:45:59 +0530

    Added Android Device Parameters

--- AndroidApp/LoginRegister/app/src/main/java/com/mobileanalytics/analytics/MainActivity.java
+++ AndroidApp/LoginRegister/app/src/main/java/com/mobileanalytics/analytics/MainActivity.java
@@ -5,6 +5,8 @@ import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Point;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 
 import java.lang.reflect.Method;
+import java.util.Calendar;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -22,7 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button bBegin,bEvent,bCrash,bEnd;
     Boolean vDeviceType;
     String[] deviceMetrics = null;
-    String vSessionID,vEmailID;
+    String vSessionID,vEmailID,vUserID,vAppKey="109158001";
+    Long vAppStart,vAppEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bEvent.setOnClickListener(this);
         bCrash.setOnClickListener(this);
         bEnd.setOnClickListener(this);
+
+        bEvent.setEnabled(false);
+        bCrash.setEnabled(false);
+        bEnd.setEnabled(false);
     }
 
     private String getResolution(){
@@ -67,11 +75,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         {
             if (emailPattern.matcher(account.name).matches() && account.type.equals("com.google"))
             {
-                System.out.println("ac name" + account.name);
                 vEmailID = account.name;
             }
         }
-        System.out.println("emails" + vEmailID);
         if (vEmailID!= null){
             return vEmailID;
         }else{
@@ -79,6 +85,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
             return vEmailID;
+    }
+
+    private Double[] getLocation(){
+        Double[] vLocation = null;
+        vLocation = new Double[2];
+        Boolean vGPSEnabled = false;
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        try {
+            try{
+                vGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            } catch(Exception ex) {}
+            if (!vGPSEnabled){
+                vLocation[0] = 0.0;
+                vLocation[1] = 0.0;
+                return vLocation;
+            }else {
+                Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                vLocation[0] = location.getLongitude();
+                vLocation[1] = location.getLatitude();
+            }
+        }catch(SecurityException s){
+            s.printStackTrace();
+        }
+
+        return vLocation;
     }
 
     private boolean getTabletDevice() {
@@ -94,6 +125,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         return false;
+    }
+
+    private String getOrientation(){
+        int vOrientation = getResources().getConfiguration().orientation;
+        if (vOrientation==1){
+            return "PORTRAIT";
+        }else{
+            return "LANDSCAPE";
+        }
     }
 
     private String getNetworkClass(Context context) {
@@ -129,28 +169,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.bBegin:
                 try{
+                    vAppStart = Calendar.getInstance().getTimeInMillis();
                     vDeviceType = getTabletDevice();
-                    System.out.println("I am here");
-                    vSessionID = Long.toString(System.currentTimeMillis()); //Objects.toString(System.currentTimeMillis(),null);
-                    System.out.println("Inside Case Statement");
-                    deviceMetrics = new String[7];
-                    System.out.println("I am 1");
+                    vUserID = getEmailID();
+                    vSessionID = Long.toString(System.currentTimeMillis());
+                    vSessionID = vSessionID.concat(vUserID);
+                    Double[] Location = getLocation();
+                    deviceMetrics = new String[11];
                     deviceMetrics[0] = vDeviceType.toString();
-                    System.out.println("I am 2");
                     deviceMetrics[1] = vSessionID;
-                    System.out.println("I am 3");
                     deviceMetrics[2] = getResolution();
-                    System.out.println("I am 4");
                     deviceMetrics[3] = getCarrier();
-                    System.out.println("I am 5");
                     deviceMetrics[4] = getNetworkClass(this);
-                    System.out.println("I am 6");
                     deviceMetrics[5] = getDeviceID();
-                    System.out.println("I am 7");
-                    deviceMetrics[6] = getEmailID();//AccountManager.get(this).getAccountsByType("com.google")[0].name;
-                    System.out.println("Inside Case Statement - 1" + vEmailID);
-                    // CALL GetText method to make post method call
+                    deviceMetrics[6] = vUserID;
+                    deviceMetrics[7] = String.valueOf(Location[0]);
+                    deviceMetrics[8] = String.valueOf(Location[1]);
+                    deviceMetrics[9] = getOrientation();
+                    deviceMetrics[10] = vAppKey;
+                    System.out.println();
                     new PostBeginData().execute(deviceMetrics);
+                    bBegin.setEnabled(false);
+                    bEvent.setEnabled(true);
+                    bCrash.setEnabled(true);
+                    bEnd.setEnabled(true);
                 }catch(Exception ex){
                     ex.printStackTrace();
                 }
@@ -158,20 +200,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.bEvent:
                 break;
             case R.id.bCrash:
-                break;
-            case R.id.bEnd:
                 try{
-                    vDeviceType = getTabletDevice();
-                    deviceMetrics = new String[7];
+                    //vDeviceType = getTabletDevice();
+                    //vUserID = getEmailID();
+                    //vSessionID = Long.toString(System.currentTimeMillis());
+                    //vSessionID = vSessionID.concat(vUserID);
+                    Double[] Location = getLocation();
+                    deviceMetrics = new String[11];
                     deviceMetrics[0] = vDeviceType.toString();
                     deviceMetrics[1] = vSessionID;
                     deviceMetrics[2] = getResolution();
                     deviceMetrics[3] = getCarrier();
                     deviceMetrics[4] = getNetworkClass(this);
                     deviceMetrics[5] = getDeviceID();
-                    deviceMetrics[6] = getEmailID();//AccountManager.get(this).getAccountsByType("com.google")[0].name;
-                    // CALL GetText method to make post method call
+                    deviceMetrics[6] = vUserID;
+                    deviceMetrics[7] = String.valueOf(Location[0]);
+                    deviceMetrics[8] = String.valueOf(Location[1]);
+                    deviceMetrics[9] = getOrientation();
+                    deviceMetrics[10] = vAppKey;
+                    System.out.println();
+                    new PostCrashData().execute(deviceMetrics);
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                }
+                break;
+            case R.id.bEnd:
+                try{
+                    Long vAppTime;
+                    vAppEnd = Calendar.getInstance().getTimeInMillis();
+                    vAppTime = vAppEnd/1000 - vAppStart/1000;
+                    vDeviceType = getTabletDevice();
+                    Double[] Location = getLocation();
+                    deviceMetrics = new String[12];
+                    deviceMetrics[0] = vDeviceType.toString();
+                    deviceMetrics[1] = vSessionID; // Its to be reused from begin session.
+                    deviceMetrics[2] = getResolution();
+                    deviceMetrics[3] = getCarrier();
+                    deviceMetrics[4] = getNetworkClass(this);
+                    deviceMetrics[5] = getDeviceID();
+                    deviceMetrics[6] = vUserID;
+                    deviceMetrics[7] = String.valueOf(Location[0]);
+                    deviceMetrics[8] = String.valueOf(Location[1]);
+                    deviceMetrics[9] = getOrientation();
+                    deviceMetrics[10] = vAppKey;
+                    deviceMetrics[11] = String.valueOf(Integer.valueOf(vAppTime.intValue()));
                     new PostEndData().execute(deviceMetrics);
+                    bBegin.setEnabled(true);
+                    bEvent.setEnabled(false);
+                    bCrash.setEnabled(false);
+                    bEnd.setEnabled(false);
                 }catch(Exception ex){
                     ex.printStackTrace();
                 }
--- AndroidApp/LoginRegister/app/src/main/java/com/mobileanalytics/analytics/PostCrashData.java
+++ AndroidApp/LoginRegister/app/src/main/java/com/mobileanalytics/analytics/PostCrashData.java
@@ -10,9 +10,9 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 
 /**
- * Created by sourabh13506 on 3/3/2016.
+ * Created by sourabh13506 on 3/4/2016.
  */
-public class PostEndData extends AsyncTask<String, String, String> {
+public class PostCrashData extends AsyncTask<String, String, String> {
     //private Exception exception;
 
     String pf="Android";
@@ -33,26 +33,27 @@ public class PostEndData extends AsyncTask<String, String, String> {
                 dtype = "Tablet";
             }
 
-            System.out.println("Inside DoInBackGround" + params[0]);
-            String data = URLEncoder.encode("pf", "UTF-8") + "=" + URLEncoder.encode(pf, "UTF-8"); //Platform
-            data += "&" + URLEncoder.encode("uid", "UTF-8") + "=" + URLEncoder.encode(params[6], "UTF-8"); //UserID
+            String data = URLEncoder.encode("pf", "UTF-8")+ "=" + URLEncoder.encode(pf, "UTF-8"); //Platform
+            data += "&" + URLEncoder.encode("uid", "UTF-8") + "=" + URLEncoder.encode(params[6], "UTF-8"); //Userid
             data += "&" + URLEncoder.encode("c", "UTF-8") + "=" + URLEncoder.encode(params[3], "UTF-8"); //Carrier
             data += "&" + URLEncoder.encode("res", "UTF-8") + "=" + URLEncoder.encode(params[2], "UTF-8"); //Resolution
-            data += "&" + URLEncoder.encode("dt", "UTF-8") + "=" + URLEncoder.encode(dtype, "UTF-8"); //DeviceType
+            data += "&" + URLEncoder.encode("dt", "UTF-8") + "=" + URLEncoder.encode(dtype, "UTF-8"); // DeviceType
             data += "&" + URLEncoder.encode("mnu", "UTF-8") + "=" + URLEncoder.encode(mnu, "UTF-8"); // Manufacturer
-            data += "&" + URLEncoder.encode("osv", "UTF-8") + "=" + URLEncoder.encode(osv, "UTF-8"); //OperatingSystemversion
+            data += "&" + URLEncoder.encode("osv", "UTF-8") + "=" + URLEncoder.encode(osv, "UTF-8"); //OperatingSystemVersion
             data += "&" + URLEncoder.encode("dev", "UTF-8") + "=" + URLEncoder.encode(dev, "UTF-8"); //DeviceModel
             data += "&" + URLEncoder.encode("avn", "UTF-8") + "=" + URLEncoder.encode(avn, "UTF-8"); //ApplicationVersion
-            data += "&" + URLEncoder.encode("lv", "UTF-8") + "=" + URLEncoder.encode(lv, "UTF-8"); // LibraryVersion
+            data += "&" + URLEncoder.encode("lv", "UTF-8") + "=" + URLEncoder.encode(lv, "UTF-8"); //LibraryVersion
             data += "&" + URLEncoder.encode("sid", "UTF-8") + "=" + URLEncoder.encode(params[1], "UTF-8"); //SessionID
-            data += "&" + URLEncoder.encode("nwk", "UTF-8") + "=" + URLEncoder.encode(params[4], "UTF-8"); //Network
-            data += "&" + URLEncoder.encode("lng", "UTF-8") + "=" + URLEncoder.encode(params[7], "UTF-8"); //Longitude
-            data += "&" + URLEncoder.encode("did", "UTF-8") + "=" + URLEncoder.encode(params[5], "UTF-8"); //DeviceID
-            data += "&" + URLEncoder.encode("lat", "UTF-8") + "=" + URLEncoder.encode(params[8], "UTF-8"); //Latitude
-            data += "&" + URLEncoder.encode("ori", "UTF-8") + "=" + URLEncoder.encode(params[9], "UTF-8"); //Orientation
+            data += "&" + URLEncoder.encode("nwk", "UTF-8") + "=" + URLEncoder.encode(params[4], "UTF-8"); // Network
+            data += "&" + URLEncoder.encode("lng", "UTF-8") + "=" + URLEncoder.encode(params[7], "UTF-8"); // Longitude
+            data += "&" + URLEncoder.encode("did", "UTF-8") + "=" + URLEncoder.encode(params[5], "UTF-8"); // DeviceID
+            data += "&" + URLEncoder.encode("lat", "UTF-8") + "=" + URLEncoder.encode(params[8], "UTF-8"); // Latitude
+            data += "&" + URLEncoder.encode("ori", "UTF-8") + "=" + URLEncoder.encode(params[9], "UTF-8"); // Orientation
             data += "&" + URLEncoder.encode("akey", "UTF-8") + "=" + URLEncoder.encode(params[10], "UTF-8"); // ApplicationKey
-            data += "&" + URLEncoder.encode("ts", "UTF-8") + "=" + URLEncoder.encode(params[11], "UTF-8"); //TimeSpent
-            data += "&" + URLEncoder.encode("rtc", "UTF-8") + "=" + URLEncoder.encode(((Long.toString(System.currentTimeMillis()/1000))), "UTF-8"); //RecordTimestampCreated
+            data += "&" + URLEncoder.encode("stkt", "UTF-8") + "=" + URLEncoder.encode("Unhandled Exception", "UTF-8"); // ApplicationKey
+            data += "&" + URLEncoder.encode("stkc", "UTF-8") + "=" + URLEncoder.encode("error.error.error", "UTF-8"); // ApplicationKey
+            data += "&" + URLEncoder.encode("stkm", "UTF-8") + "=" + URLEncoder.encode("java.lang.NullPointerException", "UTF-8"); // ApplicationKey
+            data += "&" + URLEncoder.encode("rtc", "UTF-8") + "=" + URLEncoder.encode(((Long.toString(System.currentTimeMillis()/1000))), "UTF-8"); //RecoedTimestampCreated
 
             String text = "";
             BufferedReader reader=null;
@@ -60,8 +61,7 @@ public class PostEndData extends AsyncTask<String, String, String> {
             try
             {
                 // Defined URL  where to send data
-                URL url = new URL("http://54.254.253.37/api/data/E");
-                System.out.println("Performed URL hit");
+                URL url = new URL("http://54.254.253.37/api/data/C");
                 // Send POST data request
 
                 URLConnection conn = url.openConnection();
