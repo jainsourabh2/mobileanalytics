2ce95e123e9a74a16a0705697f2c89fdb34eb6fc
root <root@ip-172-31-31-50.ap-southeast-1.compute.internal>
2016-03-26 21:26:54 +0530

    Aggregate for each database

---
+++ jobs/aggregate_event_109158001.js
@@ -0,0 +1,251 @@
+//Set the connection details
+var mongojs	= require('mongojs');
+var config	= require('../config/config');
+var dbname      = '109158001';
+var db          = mongojs(config.connectionstring + dbname);
+
+//To be incremented daily
+var endDate = new Date();
+var startDate = new Date(endDate.getTime() - 15*24*60*60*1000);
+
+var prevEventHour = '';
+var prevEventday = '';
+var prevEventWeek = '';
+var prevEventMonth = '';
+
+var connectionCount = 0;
+
+for (var dt = startDate;
+         dt <= endDate;
+         dt.setDate(dt.getDate() + 1))
+{
+
+  var aggDayQuery = {};
+  var aggWeekQuery = {};
+  var aggMonthQuery = {};
+
+  //Derive Day and Month
+  var dd = dt.getDate();
+  var mm = dt.getMonth()+1; //January is 0!
+  var yyyy = dt.getFullYear();
+  if(dd<10) {   dd='0'+dd};
+  if(mm<10) {   mm='0'+mm};
+
+  //Derive Week
+  var weekDate = new Date(dt);
+  var weekBegin = dt.getDate() - dt.getDay();
+  var weekEnd = weekBegin + 6; // last day is the first day + 6
+  var weekDay = new Date(weekDate.setDate(weekEnd));
+
+  var weekdd = weekDay.getDate();
+  var weekmm = weekDay.getMonth()+1; //January is 0!
+  var weekyyyy = weekDay.getFullYear();
+  if(weekdd<10) {   weekdd='0'+weekdd};
+  if(weekmm<10) {   weekmm='0'+weekmm};
+
+  //Derive Day, Week and Month Key values
+  var dayKey = '' + yyyy + mm + dd;
+  var weekKey = '' + weekyyyy + weekmm + weekdd;
+  var monthKey = '' + yyyy + mm;
+
+  //Derive columns to be apply aggregate functions
+  var eventDay = 'ED' + dayKey;
+  var eventWeek = 'EW' + weekKey;
+  var eventMonth ='EM' + monthKey;
+
+  function dbCloseConnection(){
+    connectionCount--;
+    if (connectionCount == 0) {
+      db.close();
+    }}
+
+  function updateAggregate(aggType, key, output) {
+    //console.log(aggType + key);
+    db.collection('agg_event_data').update
+      ({'_id.type' : aggType, '_id.key' : key}
+        ,{$set : {'value' : output}}
+        ,{upsert : true}
+       ,function (err , result) {
+           if (err || !result) {
+               console.log(err);
+               db.close();
+               return;
+          }
+        dbCloseConnection();
+        });
+      } //end of function updateAggregate
+
+  for(var hour = 0;
+          hour <= 23;
+          hour++)
+  {
+   //Derive Hour
+   var hh = hour;
+   if (hh<10) {  hh='0'+hh};
+
+   var hourKey = '' + yyyy + mm + dd + hh;
+   var eventHour = 'EH'+ hourKey;
+
+   var aggHourQuery = {};
+
+   aggHourQuery['_id'] = {'key' : hourKey
+                         ,'event' : '$' + eventHour + '.name'
+/*                         ,'resolution' : '$lr'
+                         ,'orientation' : '$lo'
+                         ,'deviceManufacturer' : '$lm'
+                         ,'deviceType' : '$lt'
+                         ,'device' : '$ld'
+                         ,'platform' : '$lp'
+                         ,'operatingSystemVersion' : '$lov'
+                         ,'appVersion' : '$lav'
+                         ,'carrier' : '$lc'
+*/                        };
+
+   var sumQuery = {$sum : '$' + eventHour + '.value'};
+   aggHourQuery['Non_Unique_Event_Count'] = sumQuery;
+   var sumQuery = {$sum : {$cond: [{$gt: ['$' + eventHour + '.value', 0]},1,0]}};
+   aggHourQuery['Unique_Event_Count'] = sumQuery;
+
+   connectionCount++;
+
+   //console.log(eventHour);
+
+   //Execute Aggregate query for Hour
+   db.collection('user_event_info').aggregate
+     ({$unwind : '$' + eventHour}
+       ,{$group: aggHourQuery}
+       ,function (err , result) {
+          if (err || !result) {
+              console.log(err);
+              db.close();
+              return;}
+
+          if (result.length > 0) {
+          //console.log(result);
+          updateAggregate('Hour', result[0]['_id']['key'], result);
+          } else dbCloseConnection();
+      }); //end of db.collection('user_event_info').aggregate
+    } //End of for loop for hourly aggregate
+
+  if (prevEventday != eventDay)
+  {
+    prevEventday = eventDay;
+    connectionCount++;
+
+    //Form Aggregate query for Day
+    aggDayQuery['_id'] = {'key' : dayKey
+                          ,'event' : '$' + eventDay + '.name'
+/*                          ,'resolution' : '$lr'
+                          ,'orientation' : '$lo'
+                          ,'deviceManufacturer' : '$lm'
+                          ,'deviceType' : '$lt'
+                          ,'device' : '$ld'
+                          ,'platform' : '$lp'
+                          ,'operatingSystemVersion' : '$lov'
+                          ,'appVersion' : '$lav'
+                          ,'carrier' : '$lc'
+*/                         };
+
+    var sumQuery = {$sum : '$' + eventDay + '.value'};
+    aggDayQuery['Non_Unique_Event_Count'] = sumQuery;
+    var sumQuery = {$sum : {$cond: [{$gt: ['$' + eventDay + '.value', 0]},1,0]}};
+    aggDayQuery['Unique_Event_Count'] = sumQuery;
+
+    //Execute Aggregate query for Day
+    db.collection('user_event_info').aggregate
+      ({$unwind : '$' + eventDay}
+        ,{$group: aggDayQuery}
+        ,function (err , result) {
+           if (err || !result) {
+               console.log(err);
+               db.close();
+               return;}
+
+           if (result.length > 0)
+           updateAggregate('Day', result[0]['_id']['key'], result);
+           else dbCloseConnection();
+       }); //end of db.collection('user_event_info').aggregate
+     } // emd of 'if' for Day Check
+
+  if (prevEventWeek != eventWeek)
+  {
+    prevEventWeek = eventWeek;
+    connectionCount++;
+
+    //Form Aggregate query for Week
+    aggWeekQuery['_id'] = {'key' : weekKey
+                          ,'event' : '$' + eventWeek + '.name'
+/*                          ,'resolution' : '$lr'
+                          ,'orientation' : '$lo'
+                          ,'deviceManufacturer' : '$lm'
+                          ,'deviceType' : '$lt'
+                          ,'device' : '$ld'
+                          ,'platform' : '$lp'
+                          ,'operatingSystemVersion' : '$lov'
+                          ,'appVersion' : '$lav'
+                          ,'carrier' : '$lc'
+*/                         };
+
+    var sumQuery = {$sum : '$' + eventWeek + '.value'};
+    aggWeekQuery['Non_Unique_Event_Count'] = sumQuery;
+    var sumQuery = {$sum : {$cond: [{$gt: ['$' + eventWeek + '.value', 0]},1,0]}};
+    aggWeekQuery['Unique_Event_Count'] = sumQuery;
+
+    //Execute Aggregate query for Week
+    db.collection('user_event_info').aggregate
+     ({$unwind : '$' + eventWeek}
+       ,{$group: aggWeekQuery}
+       ,function (err , result) {
+          if (err || !result) {
+              console.log(err);
+              db.close();
+              return;}
+
+          if (result.length > 0)
+          updateAggregate('Week', result[0]['_id']['key'], result);
+          else dbCloseConnection();
+      }); //end of db.collection('user_event_info').aggregate
+  } //end of 'if' for week check
+
+
+  if (prevEventMonth != eventMonth)
+  {
+    prevEventMonth = eventMonth;
+    connectionCount++;
+
+    //Form Aggregate query for Month
+    aggMonthQuery['_id'] = {'key' : monthKey
+                          ,'event' : '$' + eventMonth + '.name'
+/*                          ,'resolution' : '$lr'
+                          ,'orientation' : '$lo'
+                          ,'deviceManufacturer' : '$lm'
+                          ,'deviceType' : '$lt'
+                          ,'device' : '$ld'
+                          ,'platform' : '$lp'
+                          ,'operatingSystemVersion' : '$lov'
+                          ,'appVersion' : '$lav'
+                          ,'carrier' : '$lc'
+*/                         };
+
+  var sumQuery = {$sum : '$' + eventMonth + '.value'};
+  aggMonthQuery['Non_Unique_Event_Count'] = sumQuery;
+  var sumQuery = {$sum : {$cond: [{$gt: ['$' + eventMonth + '.value', 0]},1,0]}};
+  aggMonthQuery['Unique_Event_Count'] = sumQuery;
+
+  //Execute Aggregate query for Month
+  db.collection('user_event_info').aggregate
+   ({$unwind : '$' + eventMonth}
+     ,{$group: aggMonthQuery}
+     ,function (err , result) {
+        if (err || !result) {
+            console.log(err);
+            db.close();
+            return;}
+
+        if (result.length > 0)
+        updateAggregate('Month', result[0]['_id']['key'], result);
+        else dbCloseConnection();
+    }); //end of db.collection('user_event_info').aggregate
+  } //end of 'if' for month check
+
+}; //end of for loop
---
+++ jobs/aggregate_retention_109158001.js
@@ -0,0 +1,298 @@
+//Set the connection details
+//var databaseurl = 'analytics';
+var mongojs             = require('mongojs');
+//var mongoUtil = require('../connection/mongoUtil' );
+//var db                = mongoUtil.getDbMongoJS();
+var config              = require('../config/config' );
+var dbname              = '109158001';
+var db                  = mongojs(config.connectionstring + dbname);
+
+process.env.TZ = 'Asia/Kolkata';
+//To be executed for past 31 days
+var dayEndDate = new Date();
+var dayStartDate = new Date(dayEndDate.getTime() - 31*24*60*60*1000);
+//var dayStartDate = new Date(2015, 10, 02);
+//var dayEndDate = new Date(2015, 10, 02);
+
+//To be executed for past 26 weeks
+var weekEndDate = new Date();
+var weekStartDate = new Date(weekEndDate.getTime() - 180*24*60*60*1000);
+//var weekStartDate = new Date(2015, 0, 01);
+//var weekEndDate = new Date(2015, 10, 30);
+
+//To be executed for past 12 months
+var monthEndDate = new Date();
+var monthStartDate = new Date(monthEndDate.getTime() - 365*24*60*60*1000);
+//var monthStartDate = new Date(2015, 0, 01);
+//var monthEndDate = new Date(2015, 10, 30);
+
+var connectionCount = 0;
+
+function dbCloseConnection(){
+  connectionCount--;
+  if (connectionCount == 0) {
+    console.log('Connection closed');
+    db.close();
+  }}
+
+function updateAggregate(aggType, userType, key, output) {
+  //console.log(key);
+  db.collection('agg_retention_data').update
+    ({'_id.type' : aggType, '_id.user' : userType, '_id.key' : key}
+      ,{$set : {'value' : output}}
+      ,{upsert : true}
+     ,function (err , result) {
+         if (err || !result) {
+             console.log(err);
+             db.close();
+             return;
+        }
+      dbCloseConnection();
+      });
+    } //end of function updateAggregate
+
+//retention calculation for DAY
+for (var daydt = new Date(dayStartDate);
+         daydt <= dayEndDate;
+         daydt.setDate(daydt.getDate() + 1))
+{
+  //Derive Day and Month
+  var dd = daydt.getDate();
+  var mm = daydt.getMonth()+1; //January is 0!
+  var yyyy = daydt.getFullYear();
+  if(dd<10) {   dd='0'+dd};
+  if(mm<10) {   mm='0'+mm};
+
+  //Derive Day
+  var dayKey = '' + yyyy + mm + dd;
+  //console.log(dayKey);
+
+  var matchQueryNewUser = {};
+  matchQueryNewUser['fld'] = {$in : [dayKey]};
+
+  var matchQuery1ReturningUser = {};
+  matchQuery1ReturningUser['SD' + dayKey] = {$gt : 0};
+
+  var matchQuery2ReturningUser = {};
+  matchQuery2ReturningUser['fld'] = {$nin : [dayKey]};
+
+  var groupQuery = {};
+  groupQuery['_id'] = {key : dayKey};
+
+  //For each day, determine the user activity for the next 30 days
+  for (var i = 0; i<=30; i++)
+  {
+    var nextDay = new Date(daydt);
+    nextDay.setDate(daydt.getDate() + i);
+
+    var nextDay_dd = nextDay.getDate();
+    var nextDay_mm = nextDay.getMonth()+1; //January is 0!
+    var nextDay_yyyy = nextDay.getFullYear();
+    if(nextDay_dd<10) {   nextDay_dd='0'+nextDay_dd};
+    if(nextDay_mm<10) {   nextDay_mm='0'+nextDay_mm};
+
+    nextDayKey = '' + nextDay_yyyy + nextDay_mm + nextDay_dd;
+    var sessionDay = 'SD' + nextDayKey;
+    groupQuery[nextDayKey] = {$sum : {$cond: [{$gt: ['$' + sessionDay, 0]},1,0]}};
+    }
+
+  //console.log(matchQueryNewUser);
+  //console.log(groupQuery);
+
+  //Aggregate data for New User
+  connectionCount++;
+  db.collection('user_session_info').aggregate
+    ({$match : matchQueryNewUser}
+     ,{$group: groupQuery}
+//     ,{$sort : {'_id' : 1}}
+     ,function (err , result) {
+         if (err || !result) {
+             console.log(err);
+             db.close();
+             return;}
+
+        //console.log(result);
+        if(result.length > 0)
+        updateAggregate('Day', 'New', result[0]['_id']['key'], result);
+        else dbCloseConnection();
+     });
+
+   //Aggregate data for Returning User
+   connectionCount++;
+   db.collection('user_session_info').aggregate
+    ({$match : {$and : [matchQuery1ReturningUser,matchQuery2ReturningUser]}}
+     ,{$group: groupQuery}
+//     ,{$sort : {'_id' : 1}}
+     ,function (err , result) {
+        if (err || !result) {
+            console.log(err);
+            db.close();
+            return;}
+
+        //console.log(result);
+        if(result.length > 0)
+        updateAggregate('Day', 'Returning', result[0]['_id']['key'], result);
+        else dbCloseConnection();
+    });
+}
+
+//retention calculation for WEEK
+for (var weekdt = new Date(weekStartDate);
+         weekdt <= weekEndDate;
+         weekdt.setDate(weekdt.getDate() + 7))
+{
+  //Derive Week
+  var weekDate = new Date(weekdt);
+  var weekBegin = weekdt.getDate() - weekdt.getDay();
+  var weekEnd = weekBegin + 6; // last day is the first day + 6
+  var weekDay = new Date(weekDate.setDate(weekEnd));
+
+  var weekdd = weekDay.getDate();
+  var weekmm = weekDay.getMonth()+1; //January is 0!
+  var weekyyyy = weekDay.getFullYear();
+  if(weekdd<10) {   weekdd='0'+weekdd};
+  if(weekmm<10) {   weekmm='0'+weekmm};
+
+  var weekKey = '' + weekyyyy + weekmm + weekdd;
+  var deriveKey = weekyyyy + '/' + weekmm + '/' + weekdd;
+
+  var matchQueryNewUser = {};
+  matchQueryNewUser['flw'] = {$in : [weekKey]};
+
+  var matchQuery1ReturningUser = {};
+  matchQuery1ReturningUser['SW' + weekKey] = {$gt : 0};
+
+  var matchQuery2ReturningUser = {};
+  matchQuery2ReturningUser['flw'] = {$nin : [weekKey]};
+
+  var groupQuery = {};
+  groupQuery['_id'] = {key : weekKey};
+
+  //For each week, determine the user activity for the next 26 weeks
+  for (var i = 0; i<=26; i++)
+  {
+    var nextWeek = new Date(deriveKey);
+    nextWeek.setDate(nextWeek.getDate() + (i*7));
+
+    var nextWeek_dd = nextWeek.getDate();
+    var nextWeek_mm = nextWeek.getMonth()+1; //January is 0!
+    var nextWeek_yyyy = nextWeek.getFullYear();
+    if(nextWeek_dd<10) {   nextWeek_dd='0'+nextWeek_dd};
+    if(nextWeek_mm<10) {   nextWeek_mm='0'+nextWeek_mm};
+
+    nextWeekKey = '' + nextWeek_yyyy + nextWeek_mm + nextWeek_dd;
+    var sessionWeek = 'SW' + nextWeekKey;
+    groupQuery[nextWeekKey] = {$sum : {$cond: [{$gt: ['$' + sessionWeek, 0]},1,0]}};
+    }
+
+  //Aggregate data for New User
+  connectionCount++;
+  db.collection('user_session_info').aggregate
+    ({$match : matchQueryNewUser}
+     ,{$group: groupQuery}
+//     ,{$sort : {'_id' : 1}}
+     ,function (err , result) {
+         if (err || !result) {
+             console.log(err);
+             db.close();
+             return;}
+
+         if(result.length > 0)
+         updateAggregate('Week', 'New', result[0]['_id']['key'], result);
+         else dbCloseConnection();
+     });
+
+    //Aggregate data for Returning User
+    connectionCount++;
+    db.collection('user_session_info').aggregate
+     ({$match : {$and : [matchQuery1ReturningUser,matchQuery2ReturningUser]}}
+      ,{$group: groupQuery}
+//      ,{$sort : {'_id' : 1}}
+      ,function (err , result) {
+         if (err || !result) {
+             console.log(err);
+             db.close();
+             return;}
+
+         if(result.length > 0)
+         updateAggregate('Week', 'Returning', result[0]['_id']['key'], result);
+         else dbCloseConnection();
+     });
+}
+
+//retention calculation for MONTH
+for (var monthdt = new Date(monthStartDate);
+         monthdt <= monthEndDate;
+         monthdt.setDate(monthdt.getDate() + 28))
+{
+  //Derive Month
+  var mm = monthdt.getMonth()+1; //January is 0!
+  var yyyy = monthdt.getFullYear();
+  if(mm<10) {   mm='0'+mm};
+
+  var monthKey = '' + yyyy + mm;
+  var deriveKey = yyyy + '/' + mm + '/' + '01';
+
+  var matchQueryNewUser = {};
+  matchQueryNewUser['flm'] = {$in : [monthKey]};
+
+  //console.log(matchQueryNewUser);
+
+  var matchQuery1ReturningUser = {};
+  matchQuery1ReturningUser['SM' + monthKey] = {$gt : 0};
+
+  var matchQuery2ReturningUser = {};
+  matchQuery2ReturningUser['flm'] = {$nin : [monthKey]};
+
+  var groupQuery = {};
+  groupQuery['_id'] = {key : monthKey};
+
+  //For each week, determine the user activity for the next 26 weeks
+  for (var i = 0; i<=12; i++)
+  {
+    var nextMonth = new Date(deriveKey);
+    nextMonth = new Date(new Date(nextMonth).setMonth(nextMonth.getMonth()+i));
+
+    var nextMonth_mm = nextMonth.getMonth()+1; //January is 0!
+    var nextMonth_yyyy = nextMonth.getFullYear();
+    if(nextMonth_mm<10) {   nextMonth_mm='0'+nextMonth_mm};
+
+    nextMonthKey = '' + nextMonth_yyyy + nextMonth_mm;
+    var sessionMonth = 'SM' + nextMonthKey;
+    groupQuery[nextMonthKey] = {$sum : {$cond: [{$gt: ['$' + sessionMonth, 0]},1,0]}};
+    }
+
+  //Aggregate data for New User
+  connectionCount++;
+  db.collection('user_session_info').aggregate
+    ({$match : matchQueryNewUser}
+     ,{$group: groupQuery}
+//     ,{$sort : {'_id' : 1}}
+     ,function (err , result) {
+         if (err || !result) {
+             console.log(err);
+             db.close();
+             return;}
+
+         if(result.length > 0)
+         updateAggregate('Month', 'New', result[0]['_id']['key'], result);
+         else dbCloseConnection();
+     });
+
+    //Aggregate data for Returning User
+    connectionCount++;
+    db.collection('user_session_info').aggregate
+     ({$match : {$and : [matchQuery1ReturningUser,matchQuery2ReturningUser]}}
+      ,{$group: groupQuery}
+//      ,{$sort : {'_id' : 1}}
+      ,function (err , result) {
+         if (err || !result) {
+             console.log(err);
+             db.close();
+             return;}
+
+         if(result.length > 0)
+         updateAggregate('Month', 'Returning', result[0]['_id']['key'], result);
+         else dbCloseConnection();
+     });
+}
